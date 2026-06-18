a5c27050bf1a2a7f49fbe7da3f02ec5598ab8935
Ankit Dhankhar <ankit.0.prof@gmail.com>
2026-06-18 14:53:24 +0530

    tested field seperator

--- hello.R
+++ hello.R
@@ -2,3 +2,12 @@ cat("what is your name? ")
 name <- readLines("stdin", n=1)
 greeting <- paste("Hello!", name)
 print(greeting)
+print("//////")
+
+# can modify field seperator in paste
+greet1 <- paste("Yo !!!", name, sep="--")
+print(greet1)
+
+# if by default want to make sep = "" then use paste0
+greet2 <- paste0("Yupp !!!", name)
+print(greet2)
